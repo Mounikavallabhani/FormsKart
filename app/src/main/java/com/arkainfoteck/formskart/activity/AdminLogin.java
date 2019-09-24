0bdd3774884df5577f7c04c70506fe9b96925eba
Admin <mvallabhani@gmail.com>
2019-09-24 15:08:38 +0530

    Initial commit

---
+++ app/src/main/java/com/arkainfoteck/formskart/activity/AdminLogin.java
@@ -0,0 +1,16 @@
+package com.arkainfoteck.formskart.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arkainfoteck.formskart.R;
+
+public class AdminLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
---
+++ app/src/main/java/com/arkainfoteck/formskart/activity/StudentLogin.java
@@ -0,0 +1,16 @@
+package com.arkainfoteck.formskart.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arkainfoteck.formskart.R;
+
+public class StudentLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+    }
+}
